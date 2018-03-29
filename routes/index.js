65054b56ac12a0a2be3dfba7f6775d0f9631c6cb
Philip Bjerknes <philleif@Philips-iMac-Pro.local>
2018-03-29 09:50:13 -0400

    style pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy');
+});
+
+router.get('/terms', function(req, res, next) {
+  res.render('terms');
 });
 
 module.exports = router;
